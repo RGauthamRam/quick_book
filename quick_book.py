9b32b412f4969745778683608207a7dde438214a
RGauthamRam <30320759+RGauthamRam@users.noreply.github.com>
2018-11-12 22:51:00 +0530

    Update quick_book.py

--- quick_book.py
+++ quick_book.py
@@ -6,17 +6,19 @@ from bs4 import BeautifulSoup
 import requests
 
 # my Twilio phone number
-TWILIO_PHONE_NUMBER = "+16204077156"
+TWILIO_PHONE_NUMBER = ""
 
 # list of phone numbers to dial
-DIAL_NUMBERS = ["+919941803494",]
+DIAL_NUMBERS = ["",]
 
 #no idea what this does lol --- I guess instructions for when someone picks up the call
 TWIML_INSTRUCTIONS_URL = \
   "http://static.fullstackpython.com/phone-calls-python.xml"
 
 #get client from my auth token and password
-client = Client("AC50a56fe5fda22e1b771472309314c494", "3d790470d217ba0a5fc1dcfb8f1289a7")
+key = ""
+password = ""
+client = Client(key, password)
 
 def dial_numbers(numbers_list):
     """Dials one or more phone numbers from a Twilio phone number."""
