f4c5d6c28bc477101792ac9ba02ce2e9ac1b5b78
RGauthamRam <30320759+RGauthamRam@users.noreply.github.com>
2018-11-12 22:48:02 +0530

    Update callme.py

--- callme.py
+++ callme.py
@@ -3,10 +3,10 @@ from twilio.rest import TwilioRestClient
 
 # Twilio phone number goes here. Grab one at https://twilio.com/try-twilio
 # and use the E.164 format, for example: "+12025551234"
-TWILIO_PHONE_NUMBER = "+16204077156"
+TWILIO_PHONE_NUMBER = ""
 
 # list of one or more phone numbers to dial, in "+19732644210" format
-DIAL_NUMBERS = ["+919941803494",]
+DIAL_NUMBERS = ["",]
 
 # URL location of TwiML instructions for how to handle the phone call
 TWIML_INSTRUCTIONS_URL = \
@@ -14,7 +14,9 @@ TWIML_INSTRUCTIONS_URL = \
 
 # replace the placeholder values with your Account SID and Auth Token
 # found on the Twilio Console: https://www.twilio.com/console
-client = TwilioRestClient("AC50a56fe5fda22e1b771472309314c494", "3d790470d217ba0a5fc1dcfb8f1289a7")
+key = ""
+password = ""
+client = TwilioRestClient(key, password)
 
 
 def dial_numbers(numbers_list):
